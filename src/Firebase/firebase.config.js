6991ada8571996e5be6761d3cd3bce36a8ca4d56
ar-saad <arsaad.dev@gmail.com>
2025-09-20 18:46:59 +0600

    updated server url and firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCqxmdyuzXDKwAN3SXngEz3ECk6QH44TMI",
+  authDomain: "travellers-hut-100912.firebaseapp.com",
+  projectId: "travellers-hut-100912",
+  storageBucket: "travellers-hut-100912.firebasestorage.app",
+  messagingSenderId: "12548682890",
+  appId: "1:12548682890:web:e63ea93dcc670d666b6973",
 };
 
 export default firebaseConfig;
--- src/Pages/Home/Blogs/Blogs/Blogs.js
+++ src/Pages/Home/Blogs/Blogs/Blogs.js
@@ -4,61 +4,59 @@ import SingleBlog from "./../SingleBlog/SingleBlog";
 import "./Blog.css";
 
 const Blogs = () => {
-    const [blogsLoading, setBlogsLoading] = useState(false);
-    const [blogs, setBlogs] = useState([]);
-    const [page, setPage] = useState(0);
-    const [pageCount, setPageCount] = useState(0);
-    const size = 10;
+  const [blogsLoading, setBlogsLoading] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+  const [page, setPage] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const size = 10;
 
-    useEffect(() => {
-        setBlogsLoading(true);
-        fetch(
-            `https://tranquil-thicket-16665.herokuapp.com/blogs?page=${page}&&size=${size}`
-        )
-            .then((res) => res.json())
-            .then((data) => {
-                setBlogs(data.blogs);
-                const count = data.count;
-                const pageNumber = Math.ceil(count / size);
-                setPageCount(pageNumber);
-                setBlogsLoading(false);
-            });
-    }, [page]);
+  useEffect(() => {
+    setBlogsLoading(true);
+    fetch(
+      `https://travellers-hut-server.vercel.app/blogs?page=${page}&&size=${size}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setBlogs(data.blogs);
+        const count = data.count;
+        const pageNumber = Math.ceil(count / size);
+        setPageCount(pageNumber);
+        setBlogsLoading(false);
+      });
+  }, [page]);
 
-    return (
-        <div className="container mx-auto my-16 relative">
-            <h2 className="text-5xl font-bold text-center my-12">All blogs</h2>
-            <div>
-                {blogsLoading ? (
-                    <div style={{ minHeight: "250px" }}>
-                        <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2">
-                            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-                        </div>
-                    </div>
-                ) : (
-                    blogs.map((blog) => (
-                        <SingleBlog key={blog._id} blog={blog} />
-                    ))
-                )}
+  return (
+    <div className="container mx-auto my-16 relative">
+      <h2 className="text-5xl font-bold text-center my-12">All blogs</h2>
+      <div>
+        {blogsLoading ? (
+          <div style={{ minHeight: "250px" }}>
+            <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2">
+              <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+            </div>
+          </div>
+        ) : (
+          blogs.map((blog) => <SingleBlog key={blog._id} blog={blog} />)
+        )}
 
-                {/* {blogs.map((blog) => (
+        {/* {blogs.map((blog) => (
                     <SingleBlog key={blog._id} blog={blog} />
                 ))} */}
 
-                <div className="pagination flex justify-center">
-                    {[...Array(pageCount).keys()].map((number) => (
-                        <button
-                            className={number === page ? "selected" : ""}
-                            key={number}
-                            onClick={() => setPage(number)}
-                        >
-                            {number}
-                        </button>
-                    ))}
-                </div>
-            </div>
+        <div className="pagination flex justify-center">
+          {[...Array(pageCount).keys()].map((number) => (
+            <button
+              className={number === page ? "selected" : ""}
+              key={number}
+              onClick={() => setPage(number)}
+            >
+              {number}
+            </button>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
